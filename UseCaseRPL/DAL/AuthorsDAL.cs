1a0ecde46fcdb0fb879f8b5f6f9d8a92df06e20c
Ernanda <Ernanda@ERNANDA-PC>
2016-09-27 15:06:26 +0700

    Menambahkan metode getAll dan GetByID

--- UseCaseRPL/DAL/AuthorsDAL.cs
+++ UseCaseRPL/DAL/AuthorsDAL.cs
@@ -2,10 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using UseCaseRPL.Models;
 
 namespace UseCaseRPL.DAL
 {
     public class AuthorsDAL
     {
+        private CommerceDbEntities db;
+        public AuthorsDAL()
+        {
+            db = new CommerceDbEntities();
+
+        }
+
+        //menampilkan semua data Authors
+        public IQueryable<Author> getAll()
+        {
+            var result = from c in db.Authors
+                         orderby c.FirstName ascending
+                         select c;
+            return result;
+        }
+
+        public Author GetByID(int id)
+        {
+            var result = (from c in db.Authors
+                         where c.AuthorID == id
+                         select c).FirstOrDefault();
+            if (result != null)
+                return result;
+            else
+                throw new Exception("Data dengan id " + id.ToString() + " tidak ditemukan");
+        }
+
+        
     }
 }
